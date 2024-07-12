0466d64140f223335784dc49c27f10dbe82a2a19
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-07-12 12:55:46 +0200

    full 1.21 port

--- 1.21/Common/src/main/java/fuzs/leavesbegone/world/level/chunk/RandomBlockTickerChunk.java
+++ 1.21/Common/src/main/java/fuzs/leavesbegone/world/level/chunk/RandomBlockTickerChunk.java
@@ -1,9 +1,11 @@
 package fuzs.leavesbegone.world.level.chunk;
 
+import fuzs.leavesbegone.LeavesBeGone;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.ticks.LevelChunkTicks;
 
 public interface RandomBlockTickerChunk {
+    String KEY_RANDOM_BLOCK_TICKS = LeavesBeGone.id("random_block_ticks").toString();
 
     LevelChunkTicks<Block> leavesbegone$getRandomBlockTicks();
 
