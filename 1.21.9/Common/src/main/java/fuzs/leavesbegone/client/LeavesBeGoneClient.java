3e7e01615799004ceba72060b5b52b726da0a45d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 22:58:46 +0200

    prepare 1.21.10 port

--- 1.21.9/Common/src/main/java/fuzs/leavesbegone/client/LeavesBeGoneClient.java
+++
@@ -1,7 +0,0 @@
-package fuzs.leavesbegone.client;
-
-import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
-
-public class LeavesBeGoneClient implements ClientModConstructor {
-
-}
--- 1.21.9/Common/src/main/java/fuzs/leavesbegone/server/level/RandomBlockTickerLevel.java
+++
@@ -1,9 +0,0 @@
-package fuzs.leavesbegone.server.level;
-
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.ticks.LevelTicks;
-
-public interface RandomBlockTickerLevel {
-
-    LevelTicks<Block> leavesbegone$getRandomBlockTicks();
-}
