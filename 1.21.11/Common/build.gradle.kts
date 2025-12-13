49569629435e1431ecda83aba30e257c78b27aae
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-13 20:44:37 +0100

    upgrade 1.21.11 workspace

--- 1.21.11/Common/build.gradle.kts
+++ 1.21.11/Common/build.gradle.kts
@@ -5,3 +5,15 @@ plugins {
 dependencies {
     modCompileOnlyApi(libs.puzzleslib.common)
 }
+
+multiloader {
+    mixins {
+        mixin(
+            "ChunkAccess\u0024PackedTicksMixin",
+            "LeavesBlockMixin",
+            "LevelChunkMixin",
+            "SerializableChunkDataMixin",
+            "ServerLevelMixin"
+        )
+    }
+}
